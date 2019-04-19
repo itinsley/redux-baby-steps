58cb88b9b383588c6d298b13aee166b8e9837bd1
Ian <itinsley@gmail.com>
2019-04-19 14:37:56 +1000

    Turn app into component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+          <h1>Hello World</h1>
+      </div>
+    );  
+  }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
