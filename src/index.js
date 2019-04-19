4c92398835494c3e2cfa15c659092378287ec1dc
Ian <itinsley@gmail.com>
2019-04-19 14:40:18 +1000

    Render component data using state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      placeInUniverse: 'World'
+    }
+  }
+
   render(){
     return (
       <div className="App">
-          <h1>Hello World</h1>
+          <h1>Hello {this.state.placeInUniverse}</h1>
       </div>
     );  
   }
