fbb2956f3b1483249573c4febe1f2f8edc0da91e
Ian <itinsley@gmail.com>
2019-04-19 15:06:05 +1000

    Use hardcoded state to render dynamic content

--- src/index.js
+++ src/index.js
@@ -5,14 +5,45 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      placeInUniverse: 'World'
+      placeInUniverse: 'World',
+      photosFromTheWorld:[
+        {
+          "format": "jpeg",
+          "width": 5616,
+          "height": 3744,
+          "filename": "0000_yC-Yzbqy7PY.jpeg",
+          "id": 0,
+          "author": "Alejandro Escamilla",
+          "author_url": "https://unsplash.com/@alejandroescamilla",
+          "post_url": "https://unsplash.com/photos/yC-Yzbqy7PY"
+        },
+        {
+          "format": "jpeg",
+          "width": 5616,
+          "height": 3744,
+          "filename": "0001_LNRyGwIJr5c.jpeg",
+          "id": 1,
+          "author": "Alejandro Escamilla",
+          "author_url": "https://unsplash.com/@alejandroescamilla",
+          "post_url": "https://unsplash.com/photos/LNRyGwIJr5c"
+        },
+      ]
     }
   }
 
   render(){
     return (
-      <div className="App">
+      <div className="App" style={{listStyle: 'none'}}>
           <h1>Hello {this.state.placeInUniverse}</h1>
+          <h2>Some photos from around the world</h2>
+          <div>State hardcoded from <a href='https://picsum.photos/'>api</a></div>
+          <li>
+          {
+            this.state.photosFromTheWorld.map((photo)=>{
+              return  <ul><img src={`https://picsum.photos/200/300?image=${photo.id}`} /></ul>
+            })
+          }
+          </li>
       </div>
     );  
   }
